d9cd6830e34541404f7ff2eb790fd2e992dd9ba6
longxinlx <longxin_lx@163.com>
2020-11-22 17:22:54 +0800

    fix bug
    包含空格的文件名打印会截断处理

--- extract_mp3_from_video.sh
+++ extract_mp3_from_video.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
 path=$1
 files=$(ls $path)
 for fullname in $files
@@ -6,8 +8,9 @@ do
    filename=$(echo $fullname | cut -d . -f1)
    extension=$(echo $fullname | cut -d . -f2)
 if [ "$extension" == "flv" -o "$extension" == "mp4" ] ; then
-   /Users/yiming/longxinlx/ffmpeg-20180826-3aacb0d-macos64-static/bin/ffmpeg -i $fullname -f mp3 -vn $filename".mp3"
+   /Users/yiming/longxinlx/ffmpeg-20200504-5767a2e-macos64-static/bin/ffmpeg -i $fullname -f mp3 -vn $filename".mp3"
 else
     echo "不是视频文件."
 fi
 done
+IFS=$SAVEIFS
